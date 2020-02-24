fbc3b81b80a3caaea413461db8e551d14cb617b5
Dave Kane <davelkane@gmail.com>
2020-02-23 21:34:45 -0600

    added horizontal padding to content div in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ export default ({ children }) => {
   return (
     <>
       <NTHeader />
-      <div className="antialiased text-gray-900 flex flex-col items-center justify-center">
+      <div className="antialiased text-gray-900 flex flex-col items-center justify-center px-3">
         {children}
       </div>
     </>
