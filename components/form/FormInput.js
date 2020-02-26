b138391c6fed243515ad8a515ca9480743cd6eb9
Dave Kane <davelkane@gmail.com>
2020-02-25 23:23:41 -0600

    changed padding on input

--- components/form/FormInput.js
+++ components/form/FormInput.js
@@ -5,7 +5,7 @@ export default function FormInput({ content, placeHolder, type }) {
         <label className="text-xs bg-white p-1">{content}</label>
       </div>
       <input
-        className="w-full border border-gray-400 rounded pl-2 py-2"
+        className="w-full border border-gray-400 rounded pl-2 py-3"
         placeholder={placeHolder}
         type={type}
       />
