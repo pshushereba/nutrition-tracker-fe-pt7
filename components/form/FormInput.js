5e846ee8059e93b451678d7848059685b2559358
Dave Kane <davelkane@gmail.com>
2020-02-25 23:13:53 -0600

    adjusted label padding

--- components/form/FormInput.js
+++ components/form/FormInput.js
@@ -2,7 +2,7 @@ export default function FormInput({ content, placeHolder, type }) {
   return (
     <div className="flex flex-col my-2">
       <div className="-mb-3 z-0 pl-2">
-        <label className="text-xs bg-white">{content}</label>
+        <label className="text-xs bg-white p-1">{content}</label>
       </div>
       <input
         className="w-full border border-gray-400 rounded pl-2 py-2"
