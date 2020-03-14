b2d3b3ffebb69b05865ec077f7ac89effc9bc3ae
Dave Kane <davelkane@gmail.com>
2020-03-14 08:16:58 -0500

    Starting layout for dash

---
+++ components/Layout/Header.js
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+
+import MenuSVG from "./svg/MenuSVG";
+
+const AppHeader = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex w-full text-xl bg-gray-900 pt-8 pb-4 px-4">
+      <MenuSVG />
+      <div className="flex w-full justify-center">
+        {/* <div className="flex">
+          <a className="p-2">Home</a>
+          <a className="p-2">Badges</a>
+          <a className="p-2">Journal</a>
+          <a className="p-2">Community</a>
+        </div> */}
+        <h1 className="title text-white justify-center">
+          {router.pathname === "/" ? "" : "NUTRIVURV"}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default AppHeader;
