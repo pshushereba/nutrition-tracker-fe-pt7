4dd10769eed7cba2bbaae3a5fa0a9fc9eb87963b
Dave Kane <davelkane@gmail.com>
2020-03-19 16:38:24 -0500

    alt dropdown being used as a filter

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Dropdown extends Component {
-    constructor(props) {
-        super(props);
-        // Sets menu to hidden by default
-        this.state = {
-            showMenu: false
-        }
-        // Set up clickhandler for display
-        this.showMenu = this.showMenu.bind(this);
-        // Set up clickhandler for hide
-        this.closeMenu = this.closeMenu.bind(this);
-        this.handleSelect = this.handleSelect.bind(this);
+  constructor(props) {
+    super(props);
+    // Sets menu to hidden by default
+    this.state = {
+      showMenu: false
+    };
+    // Set up clickhandler for display
+    this.showMenu = this.showMenu.bind(this);
+    // Set up clickhandler for hide
+    this.closeMenu = this.closeMenu.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
 
-        this.foodObj={ measureURI: null }
+    this.foodObj = { measureURI: null };
 
-        this.filterDropdown = props.filterDropdown
-        }
-    
-      
-        // Displays menu and adds event listener to close
-    showMenu(event) {
-        event.preventDefault();
-        this.setState({ showMenu: true }, () => {
-          document.addEventListener('click', this.closeMenu);
-        });
-      }
-        // Hides menu and removes event listener
-      closeMenu(event) {
-        // Check to make sure click does not originate inside the menu
-          if (!this.dropdownMenu.contains(event.target))
-        {
-        this.setState({ showMenu: false }, () => {
-          document.removeEventListener('click', this.closeMenu);
-        });
-      }
+    this.filterDropdown = props.filterDropdown;
+  }
+
+  // Displays menu and adds event listener to close
+  showMenu(event) {
+    event.preventDefault();
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.closeMenu);
+    });
+  }
+  // Hides menu and removes event listener
+  closeMenu(event) {
+    // Check to make sure click does not originate inside the menu
+    if (!this.dropdownMenu.contains(event.target)) {
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener("click", this.closeMenu);
+      });
     }
-    handleSelect = (e) => {
-      this.props.setFoodObj({...this.props.foodObj, measureURI: e.value })
-    }
-  
+  }
+  handleSelect = e => {
+    this.props.setFoodObj({ ...this.props.foodObj, measureURI: e.value });
+  };
 
-      /* Expects a JSON object structured like
+  /* Expects a JSON object structured like
        const filterDropdown = 
        {dropName: 'somestringtoshowbeforehtemenudropsdown'
           options: [
@@ -51,30 +47,36 @@ class Dropdown extends Component {
               label: 'sometexttodisplaytotheuser',
               value: 'somevaluetopasstotheapp'
             }
-        ]} */          
+        ]} */
 
-    render() {
+  render() {
     return (
       <div>
-        <button onClick={this.showMenu}>
-          {this.filterDropdown.dropName}
-        </button>
-        { this.state.showMenu
-        ? (
-        // Adds ref to element so clicking a menu item doesn't close it
-        <div className="menu" ref={(element) => {
-            this.dropdownMenu = element;
-          }}>
-             {this.filterDropdown.options.map(cv => {
-          return <button foodObj = {this.foodObj} data={cv} key={cv.id} value = {cv.value} onClick={e => this.handleSelect(e)}> {cv.label} </button>;
-        })}
-
-        </div>
-        )
-        : (
-            null
-        )
-        }
+        <button onClick={this.showMenu}>{this.filterDropdown.dropName}</button>
+        {this.state.showMenu ? (
+          // Adds ref to element so clicking a menu item doesn't close it
+          <div
+            className="menu"
+            ref={element => {
+              this.dropdownMenu = element;
+            }}
+          >
+            {this.filterDropdown.options.map(cv => {
+              return (
+                <button
+                  foodObj={this.foodObj}
+                  data={cv}
+                  key={cv.id}
+                  value={cv.value}
+                  onClick={e => this.handleSelect(e)}
+                >
+                  {" "}
+                  {cv.label}{" "}
+                </button>
+              );
+            })}
+          </div>
+        ) : null}
       </div>
     );
   }
--- components/ingredients/IngredientSearch.js
+++ components/ingredients/IngredientSearch.js
@@ -1,95 +1,108 @@
-import React, {useState} from 'react';
-import fetch from 'isomorphic-unfetch';
-import IngredientCard from './IngredientCard.js';
-import Dropdown from '../DropdownFunc'
+import React, { useState, useEffect } from "react";
+import fetch from "isomorphic-unfetch";
+import IngredientCard from "./IngredientCard.js";
+import Dropdown from "../DropdownFunc";
+import FormDropdown from "../form/FormDropdown";
 
 const IngredientSearch = () => {
-    const [item, setItem] = useState("")
-    const [results, setResults] = useState([])
-    const [filter, setFilter] = useState("")
-    const query = item.replace(' ', '%20');
+  const [item, setItem] = useState("");
+  const [results, setResults] = useState([]);
+  const [filter, setFilter] = useState("");
+  const query = item.replace(" ", "%20");
 
-   // Object for FilterDropdown
-   const filterDropdown = {
-       dropName: "Diet",
-       options: [
-           
-        {
-           id: 1,
-           field: 'health',
-           label:'Dairy Free',
-           value: 'dairy-free'
-        },
-        
-        {
-            id: 2,
-            field: 'health',
-            label: 'Keto',
-            value: 'keto-friendly'
-         },
-         
-         {
-            id:3,
-            field: 'health',
-            label: 'Gluten Free',
-            value: 'gluten-free'
-          },
-        
-        {   id: 4,
-            field: 'health',
-            label: 'Vegan',
-            value: 'vegan'}]
-   }
+  useEffect(() => {
+    console.log("IngredientSearch: useEffect: filter=", filter);
+  }, [filter]);
 
-    const handleChange = (e) => {
-        setItem(e.target.value)
-    }
- 
-    const search = async () => {
-        let list;
- 
+  // Object for FilterDropdown
+  const filterDropdown = {
+    dropName: "Diet",
+    options: [
+      {
+        id: 1,
+        field: "health",
+        label: "Dairy Free",
+        value: "dairy-free"
+      },
+
+      {
+        id: 2,
+        field: "health",
+        label: "Keto",
+        value: "keto-friendly"
+      },
+
+      {
+        id: 3,
+        field: "health",
+        label: "Gluten Free",
+        value: "gluten-free"
+      },
+
+      { id: 4, field: "health", label: "Vegan", value: "vegan" }
+    ]
+  };
+
+  const handleChange = e => {
+    setItem(e.target.value);
+  };
+
+  const search = async () => {
+    let list;
+
+    const field = "health";
+
     // Checks if filter is active and appends to API call if needed
-        const response = await fetch(`https://api.edamam.com/api/food-database/parser?app_id=8de772d5&app_key=${process.env.FOOD_DB_KEY}&ingr=${query}${filter ? field + `=` + filter : ''}`)
-        list = await response.json();
-        return setResults(list.hints);
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        search();
-    }
+    const response = await fetch(
+      `https://api.edamam.com/api/food-database/parser?app_id=8de772d5&app_key=ba31a7a9230043a9bc36135b1a432184&ingr=${query}&${
+        filter ? `${field}=${filter}` : ""
+      }`
+    );
+    list = await response.json();
+    return setResults(list.hints);
+  };
 
-    return (
-        <div>
-            <div>
-                <h2>Search for a Food</h2>
-                <Dropdown filterDropdown={filterDropdown} setFilter={setFilter}/>
-                <input 
-                    type="text"
-                    placeholder="Search..."
-                    name="food"
-                    onChange={handleChange}></input>
-                <button 
-                    className="text-xs text-white px-6 py-1 bg-indigo-500 border-indigo-500 rounded m-2"
-                    onClick={handleSubmit}
-                >Search</button>
-            </div>
-            {results.length > 0 ? 
-            <div className="flex justify-around p-4">
-                <p className="px-4">Food Item</p>
-                <p className="px-4">Serving Size</p>
-                <p className="px-4">Fats %</p>
-                <p className="px-4">Protein %</p>
-                <p className="px-4">Carb %</p>
-                <p className="px-4">Quick View</p>
-            </div> : null}
-            <div>
-                {results.map((item) => {
-                  return <IngredientCard key={item.food.foodId} details={item} />
-                })}
-            </div>
+  const handleSubmit = e => {
+    e.preventDefault();
+    search();
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Search for a Food</h2>
+        <FormDropdown data={filterDropdown} setValue={setFilter} obj={filter} />
+        {/* <Dropdown filterDropdown={filterDropdown} setFilter={setFilter}/> */}
+        <input
+          type="text"
+          placeholder="Search..."
+          name="food"
+          onChange={handleChange}
+        ></input>
+        <button
+          className="text-xs text-white px-6 py-1 bg-indigo-500 border-indigo-500 rounded m-2"
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
+      </div>
+      {results.length > 0 ? (
+        <div className="flex justify-around p-4">
+          <p className="px-4">Food Item</p>
+          <p className="px-4">Serving Size</p>
+          <p className="px-4">Fats %</p>
+          <p className="px-4">Protein %</p>
+          <p className="px-4">Carb %</p>
+          <p className="px-4">Quick View</p>
         </div>
-    )
-}
+      ) : null}
+      <div>
+        {results.map(item => {
+          return <IngredientCard key={item.food.foodId} details={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default IngredientSearch;
