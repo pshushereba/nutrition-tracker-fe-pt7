9b31d7fa6d9745c1413429a05a7c050ba8f8abd1
Dave Kane <davelkane@gmail.com>
2020-03-19 08:25:25 -0500

    Fixed uncaught mistakes during rebase

--- components/SearchResultCard.js
+++ components/SearchResultCard.js
@@ -7,6 +7,7 @@ import FormDropdown from './form/FormDropdown';
 
 const SearchResultsCard = ({ data, setNutrInfo }) => {
     const [foodObj, setFoodObj] = useState()
+    console.log("SearchResultMaster", data)
 
 
     const search = async () => {
@@ -34,7 +35,7 @@ const SearchResultsCard = ({ data, setNutrInfo }) => {
             <div className="flex my-2">
                 <div className="flex justify-around w-full hover:bg-item-hover hover:opacity-50">
                     <p className="w-1/5 h-auto my-2 mx-4 self-center">{data.food.label}</p>
-                    <FormDropdown data={data} setValue={setFoodObj} name={"Serving Amount"} obj={foodObj}/>
+                    <FormDropdown data={data} setValue={setFoodObj} name={"Serving Amt."} obj={foodObj}/>
                     <p className="w-1/5 text-center">{Math.ceil(data.food.nutrients.FAT) || 0} g</p>
                     <p className="w-1/5 text-center">{Math.ceil(data.food.nutrients.PROCNT || 0)} g</p>
                     <p className="w-1/5 text-center">{Math.ceil(data.food.nutrients.CHOCDF) || 0} g</p>
--- components/form/FormDropdown.js
+++ components/form/FormDropdown.js
@@ -2,43 +2,29 @@ export default function FormDropdown({ data, name, setValue, obj }) {
   const handleChange = e => {
     console.log(e.target.value);
     setValue(e.target.value);
-    // setValue({ ...obj, quantity: 1, foodId: id, measureURI: (measure ? measure : otherMeasure) })
   };
 
-  const options = data.options.map(option => {
-    const id = option.id;
-    const label = option.label;
-    const value = option.value;
+  console.log("FormDropDown", data)
+
+  const options = data.measures.map(option =>{
+    const label = option.label
+    const id = data.food.foodId
+    const measure = option.measureURI   
+    const otherMeasure = option.uri
+
 
     return (
-      <option
-        key={id}
-        value={value}
-        label={label}
-        // info={ { quantity:1, foodId: id, measureURI: (measure ? measure : otherMeasure) } }
-      >
-        {option.label}
-      </option>
-    );
-  });
+        <option 
+            key={measure ? measure : option.uri}
+            value={label ? label.toLowerCase() : "missing"}
+            info={ { quantity:1, foodId: id, measureURI: (measure ? measure : otherMeasure) } }
+            
+        >
+            {option.label}
+        </option>
+    )
+})
 
-  // const options = data.measures.map(option =>{
-  //     const label = option.label
-  //     const id = data.food.foodId
-  //     const measure = option.measureURI
-  //     const otherMeasure = option.uri
-
-  //     return (
-  //         <option
-  //             key={measure ? measure : option.uri}
-  //             value={label ? label.toLowerCase() : "missing"}
-  //             info={ { quantity:1, foodId: id, measureURI: (measure ? measure : otherMeasure) } }
-
-  //         >
-  //             {option.label}
-  //         </option>
-  //     )
-  // })
   return (
     <select
       className="border border-gray-400 pl-4 py-2 mt-1 mb-1 bg-white w-1/5 self-center"
