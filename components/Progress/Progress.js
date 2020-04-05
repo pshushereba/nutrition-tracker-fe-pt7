db1fb7ad3579b77873d12bb41a2d1dbd082de12f
Patrick Shushereba <pshushereba@gmail.com>
2020-04-04 21:55:39 -0700

    Wired progress components.

--- components/Progress/Progress.js
+++ components/Progress/Progress.js
@@ -2,20 +2,27 @@ import React from "react";
 import ProgressCenter from "./ProgressCenter.js";
 import ProgressWeightLog from "./ProgressWeightLog.js";
 import ProgressGraph from './ProgressGraph.js';
+import { GET_WEIGHT_LOGS } from '../../gql/queries.js'
+import { useQuery } from "@apollo/react-hooks";
 
 
 const Progress = () => {
-  
+
+  const { loading, error, data } = useQuery(GET_WEIGHT_LOGS)
+
+  if (loading) return 'Loading...';
+  if (error) return `Error: ${error}`
+
   return (
     <section className="flex items-center justify-center">
       <div className="w-1/3">
-        <ProgressGraph />
+        <ProgressGraph data={data} />
       </div>
       <div className="w-1/3">
-        <ProgressCenter />
+        <ProgressCenter data={data} />
       </div>
       <div className="w-1/3">
-        <ProgressWeightLog />
+        <ProgressWeightLog data={data} />
       </div>
     </section>
   );
