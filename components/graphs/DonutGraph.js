af5d6a1958c4293feb73a1e9fc930538e1cfca88
Dave Kane <davelkane@gmail.com>
2020-04-29 06:41:19 -0500

    weight and streak added to graph

--- components/graphs/DonutGraph.js
+++ components/graphs/DonutGraph.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
+import { Spacer } from "../Layout/LayoutPrimitives";
 
 const data = {
   datasets: [
@@ -13,18 +14,29 @@ const data = {
 
 const DonutGraph = () => {
   return (
-    <div className="w-1/6 flex flex-col justify-around mt-8">
-      <div classname="w-1/12 flex flex-col">
-        <div>195 lbs</div>
-        <div></div>
+    <div className="w-1/5 flex justify-around mt-16">
+      <div className="flex flex-col -mr-16">
+        <Spacer />
+        <div className="flex items-baseline">
+          <div className="text-3xl">195</div>
+          <div>lbs</div>
+        </div>
+        <div>Weight</div>
       </div>
-      <div className="flex flex-col justify-center">
+      <div className=" w-56 flex flex-col justify-center">
         <Doughnut data={data} />
         <h2 className="w-full text-center font-semibold text-xl pt-4">
           Daily Intake
         </h2>
       </div>
-      <div classname="w-1/12"></div>
+      <div className="flex flex-col -ml-16">
+        <Spacer />
+        <div className="flex items-baseline">
+          <div className="text-3xl">12</div>
+          <div>days</div>
+        </div>
+        <div>Streak</div>
+      </div>
     </div>
   );
 };
