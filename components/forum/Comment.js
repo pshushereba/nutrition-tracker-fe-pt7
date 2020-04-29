5f47711956f46f6930e003098949c09bbe7f984c
Patrick Shushereba <pshushereba@gmail.com>
2020-04-28 21:22:08 -0700

    File cleanup and remove thread detail.

--- components/forum/Comment.js
+++ components/forum/Comment.js
@@ -1,54 +1,54 @@
-import React, { useState } from 'react';
-import { CenteredContainer, Spacer } from '../../components/Layout/LayoutPrimitives.js';
-import { useMutation } from '@apollo/react-hooks';
-import { ADD_COMMENT } from '../../gql/mutations.js';
-import { GET_POST_DETAILS, GET_POST_COMMENTS } from '../../gql/queries.js';
-import { useRouter } from 'next/router';
-
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { ADD_COMMENT } from "../../gql/mutations.js";
 
 const AddComment = (props) => {
-    // Holding the user's comment in state.
-    const [commentBody, setCommentBody] = useState("");
-    const postId = props.data.post.id;
-    
-    const [addPostComment] = useMutation(ADD_COMMENT);
+  // Holding the user's comment in state.
+  const [commentBody, setCommentBody] = useState("");
+  const postId = props.data.post.id;
 
-    const handleChange = (e) => {
-        setCommentBody(e.target.value)
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (!commentBody) {
-            alert("You didn't add a comment")
-        } else {
-            addPostComment({
-                variables: {
-                postId: postId,
-                body: commentBody
-                }
-            })
-            setCommentBody("")
-            props.refetch()
-        }
-    }
+  const [addPostComment] = useMutation(ADD_COMMENT);
 
-    return (
-            <div className="w-full my-8">
-                <p className="text-xs mb-2">Comment as {props.data.me.name}</p>
-                <form onSubmit={handleSubmit}>
-                    <textarea 
-                        rows="5" 
-                        cols="75" 
-                        value={commentBody}
-                        className="w-full bg-gray-10 resize-none"
-                        onChange={handleChange}></textarea>
-                    <button 
-                        type="submit"
-                        className="bg-cyan-300 text-white text-xs muli w-40 py-1 my-4 rounded float-right">Comment</button>
-                </form>
-            </div>
-    )
-}
+  const handleChange = (e) => {
+    setCommentBody(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!commentBody) {
+      alert("You didn't add a comment");
+    } else {
+      addPostComment({
+        variables: {
+          postId: postId,
+          body: commentBody,
+        },
+      });
+      setCommentBody("");
+      props.refetch();
+    }
+  };
+
+  return (
+    <div className="w-full my-8">
+      <p className="text-xs mb-2">Comment as {props.data.me.name}</p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          rows="5"
+          cols="75"
+          value={commentBody}
+          className="w-full bg-gray-10 resize-none"
+          onChange={handleChange}
+        ></textarea>
+        <button
+          type="submit"
+          className="bg-cyan-300 text-white text-xs muli w-40 py-1 my-4 rounded float-right"
+        >
+          Comment
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddComment;
--- components/forum/CommentCard.js
+++ components/forum/CommentCard.js
@@ -1,62 +1,61 @@
-import React, {useState} from 'react'
-import { CenteredContainer } from '../../components/Layout/LayoutPrimitives.js';
-import { useMutation } from '@apollo/react-hooks';
-import { UPDATE_COMMENT } from '../../gql/mutations.js';
-import { useFetchUser } from '../../lib/user.js';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { UPDATE_COMMENT } from "../../gql/mutations.js";
+import { useFetchUser } from "../../lib/user.js";
 
 const CommentCard = (props) => {
-    const [newComment, setNewComment] = useState(props.data.body)
-    const [isEditing, setIsEditing] = useState(false);
-    const { user } = useFetchUser()
-    console.log("In comment card", props);
+  const [newComment, setNewComment] = useState(props.data.body);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editComment] = useMutation(UPDATE_COMMENT);
+  const isOwnComment = props.data.user.id === props.me.id;
 
-    const [editComment] = useMutation(UPDATE_COMMENT);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setNewComment(e.target.value);
+  };
 
-    const isOwnComment = props.data.user.id === props.me.id;
+  const handleSubmit = () => {
+    editComment({
+      variables: {
+        id: props.data.id,
+        body: newComment,
+      },
+      optimisticResponse: {
+        __typename: "Mutation",
+        updateComment: {
+          id: props.data.id,
+          body: newComment,
+          __typename: "Comment",
+        },
+      },
+    });
+    setIsEditing(!isEditing);
+  };
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        setNewComment(e.target.value)
-    }
-
-    const handleSubmit = () => {
-        editComment({
-            variables: {
-                id: props.data.id,
-                body: newComment
-            },
-            optimisticResponse: {
-                __typename: "Mutation",
-                updateComment: {
-                    id: props.data.id,
-                    body: newComment,
-                    __typename: "Comment"
-                },
-              }
-        })
-        setIsEditing(!isEditing)
-    }
-    
-    return (
-        <div className="w-full">
-            <div className="mb-4 flex-col">
-                <p className="text-sm mb-2">{props.data.user.name}</p>
-                <p 
-                    onClick={() => isOwnComment && setIsEditing(!isEditing)}
-                    className={`${!isEditing ? "" : "hidden"} muli`}>{props.data.body}</p>
-            <div className={`${isEditing ? "" : "hidden"}`}>
-                <input
-                    value={newComment}
-                    onChange={handleChange}
-                ></input>
-            </div>
-            <button 
-                className={`${isEditing ? "" : "hidden"} bg-cyan-300 text-white text-xs muli w-12 py-1 my-4 rounded`}
-                onClick={handleSubmit}>Update</button>
-            </div>
+  return (
+    <div className="w-full">
+      <div className="mb-4 flex-col">
+        <p className="text-sm mb-2">{props.data.user.name}</p>
+        <p
+          onClick={() => isOwnComment && setIsEditing(!isEditing)}
+          className={`${!isEditing ? "" : "hidden"} muli`}
+        >
+          {props.data.body}
+        </p>
+        <div className={`${isEditing ? "" : "hidden"}`}>
+          <input value={newComment} onChange={handleChange}></input>
         </div>
-            
-    )
-}
+        <button
+          className={`${
+            isEditing ? "" : "hidden"
+          } bg-cyan-300 text-white text-xs muli w-12 py-1 my-4 rounded`}
+          onClick={handleSubmit}
+        >
+          Update
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default CommentCard;
